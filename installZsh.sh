1427e6442b545ca9efae55676d3a9f85dc830a9e
komar-23 <komar_23@wp.pl>
2025-05-29 21:09:28 +0200

    Script has been corrected.

--- installZsh.sh
+++ installZsh.sh
@@ -16,20 +16,18 @@ fi
 #   echo "${HOME}/${VIM_PACK_DIR}/vendor/start/cleverf exist."
 # fi
 
-if [ ! -f "${HOME}/${OMP_CONF_DIR/dan.omp.json" ] ; then
-  cp ./.config/omp/dan.omp.json ${HOME}/${OMP_CONF_DIR/dan.omp.json
+if [ ! -f "${HOME}/${OMP_CONF_DIR}/dan.omp.json" ] ; then
+  cp ./.config/omp/dan.omp.json ${HOME}/${OMP_CONF_DIR}/dan.omp.json
 else
   echo 'dan.omp.json file exist, backup has been created as a dan.omp.json.bkp file'
-  cp ${HOME}/${OMP_CONF_DIR/dan.omp.json ${HOME}/${OMP_CONF_DIR/dan.omp.json.bkp
-  cp ./.config/omp/dan.omp.json ${HOME}/${OMP_CONF_DIR/dan.omp.json
+  cp ${HOME}/${OMP_CONF_DIR}/dan.omp.json ${HOME}/${OMP_CONF_DIR}/dan.omp.json.bkp
+  cp ./.config/omp/dan.omp.json ${HOME}/${OMP_CONF_DIR}/dan.omp.json
 fi
 
-if [ ! -f "${HOME}/${OMP_CONF_DIR/ompvimode" ] ; then
+if [ ! -f "${HOME}/${OMP_CONF_DIR}/ompvimode" ] ; then
   cp ./.config/omp/ompvimode ${HOME}/${OMP_CONF_DIR/ompvimode
 else
   echo 'ompvimode file exist, backup has been created as a ompvimode.bkp file'
-  cp ${HOME}/${OMP_CONF_DIR/ompvimode ${HOME}/${OMP_CONF_DIR/ompvimode.bkp
-  cp ./.config/omp/ompvimode ${HOME}/${OMP_CONF_DIR/ompvimode
+  cp ${HOME}/${OMP_CONF_DIR}/ompvimode ${HOME}/${OMP_CONF_DIR}/ompvimode.bkp
+  cp ./.config/omp/ompvimode ${HOME}/${OMP_CONF_DIR}/ompvimode
 fi
-
-ompvimode
